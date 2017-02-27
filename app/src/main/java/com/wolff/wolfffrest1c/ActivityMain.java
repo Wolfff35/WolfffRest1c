1faa66621bf9bc84024b4303b371ed815aa1e152
wolfff <wolfff35@gmail.com>
2017-02-27 09:23:48 +0200

    Fragmrent menu begin

--- app/src/main/java/com/wolff/wolfffrest1c/ActivityMain.java
+++ app/src/main/java/com/wolff/wolfffrest1c/ActivityMain.java
@@ -15,6 +15,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -30,7 +31,6 @@ public class ActivityMain extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
