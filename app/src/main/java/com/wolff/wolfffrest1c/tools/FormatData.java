1edad8073af31ef8d4984e5984e1da73f831921a
wolfff <wolfff35@gmail.com>
2017-02-28 14:20:38 +0200

    Контекст в таски, настройки подключения

---
+++ app/src/main/java/com/wolff/wolfffrest1c/tools/FormatData.java
@@ -0,0 +1,14 @@
+package com.wolff.wolfffrest1c.tools;
+
+import com.wolff.wolfffrest1c.objects.WUsers;
+
+/**
+ * Created by wolff on 28.02.2017.
+ */
+
+public class FormatData {
+    public String format_user_post(String userString, WUsers user){
+
+        return null;
+    }
+}
