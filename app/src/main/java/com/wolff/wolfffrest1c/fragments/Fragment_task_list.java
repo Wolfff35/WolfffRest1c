2842341f0e1d0f86cf3781716cfb9d6f516d8b13
wolff <wolfff35@gmail.com>
2017-03-13 13:38:52 +0200

    13.03.2017 from home

--- app/src/main/java/com/wolff/wolfffrest1c/fragments/Fragment_task_list.java
+++ app/src/main/java/com/wolff/wolfffrest1c/fragments/Fragment_task_list.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.graphics.drawable.DrawerArrowDrawable;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 
